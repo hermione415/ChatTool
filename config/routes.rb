8fbd9553a3c824995e83e29c4215264ec458b626
hermione415 <rippy.t.310@gmail.com>
2021-12-18 12:00:07 +0900

    マイページの実装
    アカウント編集機能、投稿機能実装（一部）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
+  resources :posts, only: :index
   resources :rooms, only: [:index, :new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
